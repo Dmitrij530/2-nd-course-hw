ff4c97f66e3a82602595718ccb6b1631b7451f5b
Dmitrij530 <d9949268@gmail.com>
2026-04-28 22:23:36 +0300

    выполнена домашняя работа

---
+++ hw-2/hw-2.js
@@ -0,0 +1,51 @@
+//задание №1
+let a = alert("10");
+a = alert("20");
+
+//задание №2
+const yearManufacture = alert("2007");
+
+//задание №3
+const creatorName = alert("Брендан Эйх");
+
+//задание №4
+let b = 10;
+let c = 2;
+alert("Сумма: " + (b + c));        
+alert("Разность: " + (b - c));      
+alert("Произведение: " + (b * c));  
+alert("Частное: " + (b / c));
+
+//задание №5
+let result = 2 ** 5;
+alert(result);
+
+//задание №6
+let d = 9;
+let e = 2;
+alert(d % e);
+
+//задание №7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+//задание №8
+let age = prompt(`Сколько вам лет?`);
+alert(age);
+
+//задание №9
+const user = {
+    name: "Дмитрий",
+    age: "25",
+    isAdmin: "true"
+};
+
+//задание №10
+const name = prompt('Введите ваше имя:');
+alert(`Привет, ${name}!`);
