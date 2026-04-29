03cf668ac7e886c410f8f8397a93d3e9bb1dccf8
Dmitrij530 <d9949268@gmail.com>
2026-04-29 19:11:49 +0300

    исправлены недочеты

--- hw-2/hw-2.js
+++ hw-2/hw-2.js
@@ -1,19 +1,23 @@
 //задание №1
-let a = alert("10");
-a = alert("20");
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
 
 //задание №2
-const yearManufacture = alert("2007");
+const yearManufacture = 2007;
+alert(yearManufacture);
 
 //задание №3
-const creatorName = alert("Брендан Эйх");
+const creatorName = "Брендан Эйх";
+alert(creatorName);
 
 //задание №4
 let b = 10;
 let c = 2;
-alert("Сумма: " + (b + c));        
-alert("Разность: " + (b - c));      
-alert("Произведение: " + (b * c));  
+alert("Сумма: " + (b + c));
+alert("Разность: " + (b - c));
+alert("Произведение: " + (b * c));
 alert("Частное: " + (b / c));
 
 //задание №5
@@ -36,14 +40,14 @@ num--;
 alert(num);
 
 //задание №8
-let age = prompt(`Сколько вам лет?`);
-alert(age);
+let ages = prompt(`Сколько вам лет?`);
+alert(ages);
 
 //задание №9
 const user = {
     name: "Дмитрий",
-    age: "25",
-    isAdmin: "true"
+    age: 25,
+    isAdmin: true
 };
 
 //задание №10
